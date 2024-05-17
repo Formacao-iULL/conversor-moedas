04e481f504dd728afd37ed841020e8ec36193836
Ana Júlia <alvncar@gmail.com>
2024-05-17 20:46:10 -0300

    feat: currency search method implemented

--- src/app/components/listagem/listagem.component.ts
+++ src/app/components/listagem/listagem.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { CurrencyService } from '../../../services/currency.service';
 import { CommonModule } from '@angular/common';
-
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { ButtonComponent } from '../button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listagem',
@@ -15,10 +15,11 @@ import { ButtonComponent } from '../button/button.component';
     CommonModule,
     MatTableModule,
     MatPaginatorModule,
-    ButtonComponent
+    ButtonComponent,
+    FormsModule
   ],
   templateUrl: './listagem.component.html',
-  styleUrl: './listagem.component.scss'
+  styleUrls: ['./listagem.component.scss']
 })
 
 export class ListagemComponent implements OnInit {
@@ -29,6 +30,7 @@ export class ListagemComponent implements OnInit {
   pageSize: number = 10;
   pageSizeOptions: number[] = [5, 10, 25];
   pageIndex: number = 0;
+  searchText: string = '';
 
   constructor(private currencyService: CurrencyService) { }
 
@@ -37,7 +39,7 @@ export class ListagemComponent implements OnInit {
       if (data && data.supported_codes && Array.isArray(data.supported_codes)) {
         this.currencies = data.supported_codes;
         this.totalLength = this.currencies.length;
-        this.paginatedCurrencies = this.currencies.slice(0, this.pageSize); 
+        this.paginatedCurrencies = this.currencies.slice(0, this.pageSize);
       } else {
         console.error('Dados retornados não são um array:', data);
       }
@@ -45,16 +47,20 @@ export class ListagemComponent implements OnInit {
   }
 
   onPageChange(event: PageEvent) {
-    const startIndex = this.pageIndex * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
+    const startIndex = event.pageIndex * event.pageSize;
+    const endIndex = startIndex + event.pageSize;
     this.paginatedCurrencies = this.currencies.slice(startIndex, endIndex);
-    this.totalLength = this.currencies.length; 
-    this.pageSize = event.pageSize;
-
-    this.pageSize = event.pageSize;
     this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+  }
+
+  searchCurrencies() {
+    const filteredCurrencies = this.currencies.filter(currency =>
+      currency[0].toLowerCase().includes(this.searchText.toLowerCase()) ||
+      currency[1].toLowerCase().includes(this.searchText.toLowerCase())
+    );
+    this.totalLength = filteredCurrencies.length;
+    this.paginatedCurrencies = filteredCurrencies.slice(0, this.pageSize);
+    this.pageIndex = 0;
   }
 }
-
-
-
